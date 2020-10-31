726acb064f351ed9955a5165d10102b2ffa2ca6f
PriyankaP1208 <ppriyapatil1208@gmail.com>
2020-10-31 22:09:32 +0530

    Added file:To add part time Employee and Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,7 +2,9 @@
 isPresent=1
 PER_HOUR=20
 FULLDAY_HOUR=8
+PARTTIME_HOUR=8
 randomCheck=$((RANDOM%2 + 1 ))
+T=$(($RANDOM%8+1))
 if [[ $isPresent -eq $randomCheck ]]
 then 
   echo "Employee is present"
@@ -11,3 +13,5 @@ else
 fi
 dailyWage=$(( $PER_HOUR * $FULLDAY_HOUR ))
 echo "Daily Employee Wage=$dailyWage"
+partTime=$(( $T * $PARTTIME_HOUR ))
+echo "For $T hours wages are: $partTime"
