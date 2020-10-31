240871fb7dcd69e77fa5b053746cf7fa9e759145
PriyankaP1208 <ppriyapatil1208@gmail.com>
2020-10-31 22:56:46 +0530

    Added File:To tillCalculate Wages a condition of total working hours or
    days is reached for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,6 +4,9 @@ PER_HOUR=20
 FULLDAY_HOUR=8
 PARTTIME_HOUR=8
 WORKINGDAYS_PERMONTH=20
+days=0
+working_hr=0
+empHr=0
 T=$(($RANDOM%8+1))
 
 echo "1. Employee Attendance 2.Daily Employee Wage 3.Part Time wage"
@@ -30,3 +33,20 @@ esac
 
 monthlywage=$(( $PER_HOUR * $WORKINGDAYS_PERMONTH ))
 echo "Monthly Wages = $monthlywage"
+echo "Daily wages            Total wages   "
+while [[ $days -lt 20  &&  $working_hr -lt 100 ]]
+do
+	empcheck=$(($RANDOM%2+1))
+	case $empcheck in
+	1)
+		empHr=$(( $empHr + 8 ))
+	;;
+	2)
+		empHr=$(( $empHr + 4 ))
+	;;
+	esac
+salary=$(( $empHr * $PER_HOUR ))
+total_sal=$(( $total_sal + $salary ))
+((days++))
+echo "$salary                      $total_sal"
+done
