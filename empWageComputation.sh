552222a507ffded8a5514c43cbab9448277f8493
PriyankaP1208 <ppriyapatil1208@gmail.com>
2020-10-31 22:19:30 +0530

    Added File:To solving using Switch Case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,15 +3,26 @@ isPresent=1
 PER_HOUR=20
 FULLDAY_HOUR=8
 PARTTIME_HOUR=8
-randomCheck=$((RANDOM%2 + 1 ))
 T=$(($RANDOM%8+1))
-if [[ $isPresent -eq $randomCheck ]]
-then 
-  echo "Employee is present"
-else
-  echo "Employee is not present"
-fi
-dailyWage=$(( $PER_HOUR * $FULLDAY_HOUR ))
-echo "Daily Employee Wage=$dailyWage"
-partTime=$(( $T * $PARTTIME_HOUR ))
-echo "For $T hours wages are: $partTime"
+
+echo "1. Employee Attendance 2.Daily Employee Wage 3.Part Time wage"
+read n
+case $n in 
+	1)
+		randomCheck=$((RANDOM%2 + 1 ))
+		if [[ $isPresent -eq $randomCheck ]]
+		then
+  			echo "Employee is present"
+		else
+  			echo "Employee is not present"
+		fi
+	;;
+	2)
+		dailyWage=$(( $PER_HOUR * $FULLDAY_HOUR ))
+		echo "Daily Employee Wage=$dailyWage"
+	;;
+	3)
+		partTime=$(( $T * $PARTTIME_HOUR ))
+		echo "For $T hours wages are: $partTime"
+	;;
+esac
